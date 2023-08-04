8f87c0d34e6a8cb66c762b67da47f34b0932f2d0
Dahlia Guido <dguido.dev@gmail.com>
2023-08-03 19:39:13 -0500

    setting up schema

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS company_staffDB;
+CREATE DATABASE company_staffDB;
+
+USE company_staffDB;
+
+--Department
+CREATE TABLE department (
+
+);
+
+-- Role
+CREATE TABLE role (
+
+);
+
+--Employee
+CREATE TABLE employee(
+  
+)
