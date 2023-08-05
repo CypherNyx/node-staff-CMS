31327fe44906a459c397f50c233dc7460826e270
Dahlia Guido <dguido.dev@gmail.com>
2023-08-04 23:30:44 -0500

    fix table columns and seeded db

--- db/schema.sql
+++ db/schema.sql
@@ -2,29 +2,27 @@ DROP DATABASE IF EXISTS company_staff_db;
 CREATE DATABASE company_staff_db;
 
 USE company_staff_db;
-
---Department
+/*table begins here*/
 CREATE TABLE department (
-    department_id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(100) NOT NULL
+    department_id INT NOT NULL PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL     
 );
-
--- Role
-CREATE TABLE role (
-  role_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roleType (
+  role_id INT NOT NULL PRIMARY KEY,
   role_title VARCHAR(100) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
   department_id INT, 
-  FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE SET NULL
+  FOREIGN KEY (department_id) 
+  REFERENCES department(department_id) 
+  ON DELETE SET NULL
 );
 
---Employee
 CREATE TABLE employee(
-  employee_id INT AUTO_INCREMENT PRIMARY KEY,
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(200) NOT NULL,
   role_id INT,
   manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE SET NULL,
-  FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL,
+  FOREIGN KEY (role_id) REFERENCES roleType(role_id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL
 );
